a46d1f4bf47d5c83c838f28d367a258252e97b91
Andrea Gutierrez <andrea.gurod@gmail.com>
2018-01-23 00:54:52 -0600

    modificacion de estilos y estrcutura de HTMLs en general

--- js/app.js
+++ js/app.js
@@ -1,42 +1,47 @@
 var digits = "01234567";
 var longitudeCode = 3;
-
-var $phoneNumberInput = $('#phone-number');
 var $linkToVerifyView = $('.verify-phone-next');
+var $phoneNumberInput = $('#phone-number');
 
+
+// Función que carag a jQuery
 $(document).ready(loadPage);
 
-function loadPage() { // función que centraliza al resto de las funciones
+// Función central. Carga las demás funciones declaradas posteriormente
+function loadPage() {
   loadSplashView();
   loadMainView();
   $phoneNumberInput.keyup(validatePhoneNumber);
   $linkToVerifyView.click(codeGenerator);
 }
 
-function loadSplashView() { // función para mostrar la vista splash por tres segundos
+// Función del splash y su determinación de tiempo
+function loadSplashView() {
   setTimeout(function() {
     $("#view-splash").fadeOut(1500);
   },3000);
 }
 
-function loadMainView() { // función que muestra la vista principal pasado la vista splash
+// Función de primee view para usuario - Login y sign up 
+function loadMainView() {
   setTimeout(function() {
     $("#signup").fadeIn(1500);
   },3000);
 }
 
+
+// Funcion de validación de validación de numero y activación de botón
 function validatePhoneNumber() {
   var $buttonToVerifyView = $('.verify-phone');
 
   if($(this).val().trim().length === 10) {
     $buttonToVerifyView.removeAttr('disabled');
-    // $linkToVerifyView.attr("href", "verify-view.html");
   }else {
     $buttonToVerifyView.attr('disabled', true);
-    // $linkToVerifyView.attr('href','#');
   }
 }
 
+// Función de generador de codigo random para usuario 
 function codeGenerator(e){
   e.preventDefault();
   code = "";
@@ -44,7 +49,6 @@ function codeGenerator(e){
     randomNumber = Math.floor(Math.random() * digits.length);
     code += digits.substr(randomNumber, 1);
   }
-  // $linkToVerifyView.attr("href", "verify-view.html");
   alert('Your code is: LAB-' + code);
   window.location.href = "verify-view.html";
 }
